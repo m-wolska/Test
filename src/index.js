2171eaea7dafdb039f29fb13e66dce32b9165788
m-wolska <m.wolska@icloud.com>
2023-06-12 15:41:19 +0100

    changes
    file amend to a working test

---
+++ src/index.js
@@ -0,0 +1,40 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20,
+  },
+};
+
+let city = prompt("Enter a city:").trim().toLowerCase();
+
+if (city in weather) {
+  alert(
+    "It is currently " +
+      Math.round(weather[city].temp) +
+      " in " +
+      city +
+      " with a humidity of " +
+      Math.round(weather[city].humidity)
+  );
+} else {
+  alert(
+    "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+" +
+      city
+  );
+}
